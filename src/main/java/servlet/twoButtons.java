c3422865b5d3918aae7d4e26011b746988f6caf3
jennashuler <jenna.shuler@specinnovations.com>
2022-03-01 19:01:47 -0500

    two buttons

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -31,8 +31,8 @@ public class twoButtons extends HttpServlet
 {
 
 // Location of servlet.
-static String Domain  = "cs.gmu.edu:8443";
-static String Path    = "/offutt/servlet/";
+//static String Domain  = "cs.gmu.edu:8443";
+//static String Path    = "/offutt/servlet/";
 static String Servlet = "twoButtons";
 
 // Button labels
