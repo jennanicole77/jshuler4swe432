beca474616352736fec64c55e41582b02dc3cbed
jennashuler <jenna.shuler@specinnovations.com>
2022-03-07 17:23:03 -0500

    adding url for assignment 4

---
+++ src/main/java/servlet/Assignment4.java
@@ -0,0 +1,181 @@
+/** *****************************************************************
+    twoButtons.java   servlet example
+
+        @author Jeff Offutt
+********************************************************************* */
+
+// Import Java Libraries
+import java.io.*;
+import java.util.*;
+
+//Import Servlet Libraries
+import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// twoButtons class
+// CONSTRUCTOR: no constructor specified (default)
+//
+// ***************  PUBLIC OPERATIONS  **********************************
+// public void doPost ()  --> prints a blank HTML page
+// public void doGet ()  --> prints a blank HTML page
+// private void PrintHead (PrintWriter out) --> Prints the HTML head section
+// private void PrintBody (PrintWriter out) --> Prints the HTML body with
+//              the form. Fields are blank.
+// private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
+//              Prints the HTML body with the form.
+//              Fields are filled from the parameters.
+// private void PrintTail (PrintWriter out) --> Prints the HTML bottom
+//***********************************************************************
+
+@WebServlet(name = "assignment4", urlPatterns= {"/assignment4"})
+public class twoButtons extends HttpServlet
+{
+{
+
+// Location of servlet.
+//static String Domain  = "cs.gmu.edu:8443";
+//static String Path    = "/offutt/servlet/";
+static String Servlet = "assignmetn4";
+
+// Button labels
+static String OperationAdd = "Add";
+static String OperationSub = "Subtract";
+static String ConcatenateAB = "Merge string A before string B";
+static String ConcatenateBA = "Merge string B before string A";
+
+
+// Other strings.
+//static String Style ="https://www.cs.gmu.edu/~offutt/classes/432/432-style.css";
+
+/** *****************************************************
+ *  Overrides HttpServlet's doPost().
+ *  Converts the values in the form, performs the operation
+ *  indicated by the submit button, and sends the results
+ *  back to the client.
+********************************************************* */
+public void doPost (HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException
+{
+   String rslt   = "";
+   String lhsVal = "";
+   String rhsVal = "";
+   String operation = request.getParameter("Operation");
+   String lhsStr = request.getParameter("LHS");
+   String rhsStr = request.getParameter("RHS");
+   if ((lhsStr != null) && (lhsStr.length() > 0))
+      lhsVal = lhsStr;
+   if ((rhsStr != null) && (rhsStr.length() > 0))
+      rhsVal = rhsStr;
+
+   if (operation.equals(ConcatenateAB))
+   {
+      rslt = lhsVal+rhsVal;
+   }
+   else if (operation.equals(ConcatenateBA))
+   {
+      rslt = rhsVal+lhsVal;
+   }
+   
+   response.setContentType("text/html");
+   PrintWriter out = response.getWriter();
+   PrintHead(out);
+   PrintBody(out, lhsVal, rhsVal, rslt.toString());
+   PrintTail(out);
+}  // End doPost
+
+/** *****************************************************
+ *  Overrides HttpServlet's doGet().
+ *  Prints an HTML page with a blank form.
+********************************************************* */
+public void doGet (HttpServletRequest request, HttpServletResponse response)
+       throws ServletException, IOException
+{
+   response.setContentType("text/html");
+   PrintWriter out = response.getWriter();
+   PrintHead(out);
+   PrintBody(out);
+   PrintTail(out);
+} // End doGet
+
+/** *****************************************************
+ *  Prints the <head> of the HTML page, no <body>.
+********************************************************* */
+private void PrintHead (PrintWriter out)
+{
+   out.println("<html>");
+   out.println("");
+
+   out.println("<head>");
+   out.println("<title>Two buttons example</title>");
+    out.println("<script src=\"js/index.js\"/></script>");
+   out.println(" <link rel=\"stylesheet\" type=\"text/css\" href=\"" + Style + "\">");
+   out.println("</head>");
+   out.println("");
+} // End PrintHead
+
+/** *****************************************************
+ *  Prints the <BODY> of the HTML page with the form data
+ *  values from the parameters.
+********************************************************* */
+private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
+{
+   out.println("<body>");
+   out.println("<p>");
+   out.println("A simple example that demonstrates how to operate with");
+   out.println("multiple submit buttons.");
+   out.println("</p>");
+   out.print  ("<form method=\"post\"");
+   out.println(" action=\"" +Servlet + "\">");
+   out.println("");
+   out.println(" <table>");
+   out.println("  <tr>");
+   out.println("   <td>First text:");
+   out.println("   <td><input type=\"text\" name=\"LHS\" value=\"" + lhs + "\" size=30>");
+   out.println("  </tr>");
+   out.println("  <tr>");
+   out.println("   <td>Second text:");
+   out.println("   <td><input type=\"text\" name=\"RHS\" value=\"" + rhs + "\" size=30>");
+   out.println("  </tr>");
+   out.println("  <tr>");
+   out.println("   <td>Result:");
+   out.println("   <td><input type=\"text\" name=\"RHS\" value=\"" + rslt + "\" size=60>");
+   out.println("  </tr>");
+   out.println(" </table>");
+   out.println(" <br>");
+   out.println(" <br>");
+   out.println(" <input type=\"submit\" value=\"" + ConcatenateAB + "\" name=\"Operation\">");
+   out.println(" <input type=\"submit\" value=\"" + ConcatenateBA + "\" name=\"Operation\">");
+   out.println(" <input type=\"reset\" value=\"Reset\" name=\"reset\">");
+   out.println("</form>");
+   out.println("");
+   out.println("</body>");
+} // End PrintBody
+
+/** *****************************************************
+ *  Overloads PrintBody (out,lhs,rhs,rslt) to print a page
+ *  with blanks in the form fields.
+********************************************************* */
+private void PrintBody (PrintWriter out)
+{
+   PrintBody(out, "", "", "");
+}
+
+/** *****************************************************
+ *  Prints the bottom of the HTML page.
+********************************************************* */
+private void PrintTail (PrintWriter out)
+{
+   out.println("");
+   out.println("</html>");
+} // End PrintTail
+
+}  // End twoButtons
+
+
+
