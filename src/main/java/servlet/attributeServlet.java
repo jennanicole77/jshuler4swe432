7893f2eb7194a18bc19baf1868e096e8434eb38c
jennashuler <jenna.shuler@specinnovations.com>
2022-03-29 21:30:55 -0400

    invalidate

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -20,81 +20,119 @@ public class attributeServlet extends HttpServlet
 public void doGet (HttpServletRequest request, HttpServletResponse response)
        throws ServletException, IOException
 {
-   // Get session object
-   HttpSession session = request.getSession();
+   String action = request.getParameter("action");
 
-   String name   = request.getParameter("attrib_name");
-   String value  = request.getParameter("name_value");
-   String age   = request.getParameter("attrib_age");
-   String remove = request.getParameter("attrib_remove");
+   if (action != null && action.equals("invalidate"))
+   {  // Called from the invalidate button, kill the session.
+      // Get session object
+      HttpSession session = request.getSession();
+      session.invalidate();
 
-   if (remove != null && remove.equals("on"))
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      out.println("<html>");
+      out.println("<head>");
+      out.println(" <title>Session lifecycle</title>");
+      out.println("</head>");
+      out.println("");
+      out.println("<body>");
+
+      out.println("<p>Your session has been invalidated.</P>");
+
+      // Create a link so the user can create a new session.
+      // The link will have a parameter builtin
+      String lifeCycleURL = "/offutt/servlet/sessionLifeCycle";
+      out.println("<a href=\"" + lifeCycleURL + "?action=newSession\">");
+      out.println("Create new session</A>");
+
+      out.println("</body>");
+      out.println("</html>");
+      out.close();
+   } //end if
+   else 
    {
-      session.removeAttribute(name);
-   }
-   else
-   {
-      if ((name != null && name.length() > 0) && (value != null && value.length() > 0) 
-        && (age != null && age.length() >0))
+      // Get session object
+      HttpSession session = request.getSession();
+
+      String name   = request.getParameter("attrib_name");
+      String value  = request.getParameter("name_value");
+      String age   = request.getParameter("attrib_age");
+      String remove = request.getParameter("attrib_remove");
+
+      if (remove != null && remove.equals("on"))
       {
-         String[] values = {value, age};
-         session.setAttribute(name, values);
+         session.removeAttribute(name);
+      }
+      else
+      {
+         if ((name != null && name.length() > 0) && (value != null && value.length() > 0) 
+           && (age != null && age.length() >0))
+         {
+            String[] values = {value, age};
+            session.setAttribute(name, values);
+         }
+
       }
 
-   }
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
 
-   response.setContentType("text/html");
-   PrintWriter out = response.getWriter();
+      out.println("<html>");
+      // no-cache lets the page reload by clicking on the reload link
+      out.println("<meta http-equiv=\"Pragma\" content=\"no-cache\">");
+      out.println("<head>");
+      out.println(" <title>Session lifecycle</title>");
+      out.println("</head>");
+      out.println("");
 
-   out.println("<html>");
-   // no-cache lets the page reload by clicking on the reload link
-   out.println("<meta http-equiv=\"Pragma\" content=\"no-cache\">");
-   out.println("<head>");
-   out.println(" <title>Session lifecycle</title>");
-   out.println("</head>");
-   out.println("");
+      out.println("<body>");
+      out.println("<h1><center>Session attributes</center></h1>");
 
-   out.println("<body>");
-   out.println("<h1><center>Session attributes</center></h1>");
+      // String url = response.encodeURL ("offutt/servlet/attributeServlet");
+      String url = response.encodeURL("attributeServlet");
+      out.println("<form action=\"" + url + "\" method=\"GET\">");
+      out.println("Enter name, age, and value of an attribute<br>");
 
-   // String url = response.encodeURL ("offutt/servlet/attributeServlet");
-   String url = response.encodeURL("attributeServlet");
-   out.println("<form action=\"" + url + "\" method=\"GET\">");
-   out.println("Enter name, age, and value of an attribute<br>");
+      out.println(" Name: ");
+      out.println(" <input type=\"text\" size=\"10\" name=\"attrib_name\">");
 
-   out.println(" Name: ");
-   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_name\">");
-
-   out.println(" Age: ");
-   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_age\">");
+      out.println(" Age: ");
+      out.println(" <input type=\"text\" size=\"10\" name=\"attrib_age\">");
    
-   out.println(" Value: ");
-   out.println(" <input type=\"text\" size=\"10\" name=\"name_value\">");
+      out.println(" Value: ");
+      out.println(" <input type=\"text\" size=\"10\" name=\"name_value\">");
    
-   out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
-   out.println(" <input type=\"submit\" name=\"update\" value=\"Update\" age=\"Update\">");
-   out.println("</form>");
-   out.println("<hr>");
+      out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
+      out.println(" <input type=\"submit\" name=\"update\" value=\"Update\" age=\"Update\">");
+      out.println("</form>");
+      out.println("<hr>");
 
-   out.println("Attributes in this session:");
-   Enumeration e = session.getAttributeNames();
-   while (e.hasMoreElements())
-   {
-      String att_name  = (String) e.nextElement();
-      String[] vals = (String[])session.getAttribute(att_name);
-      String att_age = vals[1];
-      String att_value = vals[0];
+      out.println("Attributes in this session:");
+      Enumeration e = session.getAttributeNames();
+      while (e.hasMoreElements())
+      {
+         String att_name  = (String) e.nextElement();
+         String[] vals = (String[])session.getAttribute(att_name);
+         String att_age = vals[1];
+         String att_value = vals[0];
 
-      out.print  ("<br><b>Name:</b> ");
-      out.println(att_name);
-      out.print  ("<br><b>Value:</b> ");
-      out.println(att_value);
-      out.print  ("<br><b>Age:</b> ");
-      out.println(att_age);
-   } //end while
+         out.print  ("<br><b>Name:</b> ");
+         out.println(att_name);
+         out.print  ("<br><b>Value:</b> ");
+         out.println(att_value);
+         out.print  ("<br><b>Age:</b> ");
+         out.println(att_age);
+      } //end while
+      out.print("<br>)
 
-   out.println("</body>");
-   out.println("</html>");
-   out.close();
+      String lifeCycleURL = "/offutt/servlet/sessionLifeCycle";
+      out.print  ("<br><br><a href=\"" + lifeCycleURL + "?action=invalidate\">");
+      out.println("Invalidate the session</a>");
+
+      out.println("</body>");
+      out.println("</html>");
+      out.close();
+   }//End else
 } // End doGet
 } //End  SessionLifeCycle
